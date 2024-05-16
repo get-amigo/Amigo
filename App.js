2accf26698a85991c296be718cdb4876ddf110be
Owojori Akintunde <oneboyfromife@gmail.com>
2024-05-16 05:37:02 +0100

    Working

--- App.js
+++ App.js
@@ -4,24 +4,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigator/RootNavigator';
 import { KeyboardAvoidingView, Platform } from 'react-native';
 import FlashMessage from 'react-native-flash-message';
-import TabNavigator from './navigator/TabNavigator';
-import { NavigationContainer } from '@react-navigation/native';
 function App() {
     return (
-        <NavigationContainer>
-            <SafeAreaProvider>
-                <StatusBar style="auto" />
-                <KeyboardAvoidingView
-                    style={{ flex: 1 }}
-                    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-                    enabled
-                    keyboardVerticalOffset={-900}
-                >
-                    <RootNavigator />
-                </KeyboardAvoidingView>
-                <FlashMessage position="top" duration={2000} />
-            </SafeAreaProvider>
-        </NavigationContainer>
+        <SafeAreaProvider>
+            <StatusBar style="auto" />
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled
+            keyboardVerticalOffset={-900}>
+                <RootNavigator />
+            </KeyboardAvoidingView>
+            <FlashMessage position="top" duration={2000} />
+        </SafeAreaProvider>
     );
 }
 

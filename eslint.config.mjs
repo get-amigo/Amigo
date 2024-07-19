1c1fec2ef6e48c29cd5ea058dec175ff5d02b7e4
Tirthajyoti Ghosh <itirthahere@gmail.com>
2024-07-19 15:01:47 +0530

    fix(eslint): fix eslint parser error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
+import { fixupConfigRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import reactPlugin from 'eslint-plugin-react';
+import babelParser from '@babel/eslint-parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,12 +14,14 @@ const compat = new FlatCompat({
 });
 
 export default [
-    ...fixupConfigRules(compat.extends('universe/native')),
-
     {
-        plugins: {
-            react: fixupPluginRules(reactPlugin),
-        },
         ignores: ['node_modules', '.gen', '.expo', '.idea', 'assets'],
+    },
+    ...fixupConfigRules(...compat.extends('universe/native')),
+    {
+        files: ['**/*.js', '**/*.mjs', '**/*.jsx'],
+        languageOptions: {
+            parser: babelParser,
+        },
     },
 ];

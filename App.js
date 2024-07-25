ca9dbbe6eb877b1a00ddc46871c179ecc5f61032
SHASHWAT-SGH <vnsshashwat@gmail.com>
2024-07-25 14:57:39 +0530

    removed splash screen auto hide

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { KeyboardAvoidingView, Platform } from 'react-native';
 import FlashMessage from 'react-native-flash-message';
@@ -6,6 +7,12 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-get-random-values';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigator/RootNavigator';
+
+SplashScreen.preventAutoHideAsync().catch(() => {});
+
+async function hideSplashScreen() {
+    await SplashScreen.hideAsync();
+}
 
 Sentry.init({
     dsn: 'https://5e35d45895f220b8681a2ce7bb0728df@o4507295198085120.ingest.us.sentry.io/4507295216762880',
@@ -22,6 +29,7 @@ function App() {
             <SafeAreaProvider>
                 <StatusBar style="auto" />
                 <KeyboardAvoidingView
+                    onLayout={hideSplashScreen}
                     style={{ flex: 1 }}
                     behavior={Platform.OS === 'ios' ? 'padding' : undefined}
                     enabled
